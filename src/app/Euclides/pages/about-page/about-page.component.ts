19bdefb5188625e257904fe1cce2cea202477e5d
euclidesseg <euclides2696@gmail.com>
2026-09-10 00:11:29 -0500

    add section of focus areas

--- src/app/Euclides/pages/about-page/about-page.component.ts
+++ src/app/Euclides/pages/about-page/about-page.component.ts
@@ -1,8 +1,9 @@
 import { UpperCasePipe } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FocusAreasComponent } from "../../components/focus-areas/focus-areas.component";
 
 @Component({
-  imports: [UpperCasePipe],
+  imports: [UpperCasePipe, FocusAreasComponent],
   templateUrl: './about-page.component.html',
   styleUrl:'./about-page.component.css'
 })
