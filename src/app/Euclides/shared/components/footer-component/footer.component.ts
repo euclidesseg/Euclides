eec8dcf9baad1baeff5500b33e143e037c9d882e
euclidesseg <euclides2696@gmail.com>
2026-09-15 18:35:13 -0500

    add footer design

--- src/app/Euclides/shared/components/footer-component/footer.component.ts
+++ src/app/Euclides/shared/components/footer-component/footer.component.ts
@@ -1,11 +1,12 @@
 import { UpperCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { ParticleCanvasComponent } from '../../../components/particle-canva-component/particle-canvas.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-footer-component',
-  imports: [UpperCasePipe, ParticleCanvasComponent],
+  imports: [RouterLink, ParticleCanvasComponent],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css',
 })
-export class FooterComponent {}
+export class FooterComponent { }
