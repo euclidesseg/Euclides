75f4552a5f972cb5b1a719d8c69874b8d6c641ac
euclidesseg <euclides2696@gmail.com>
2026-09-21 17:47:48 -0500

    update contact section

--- src/app/Euclides/pages/contact-page/contact-page.component.ts
+++ src/app/Euclides/pages/contact-page/contact-page.component.ts
@@ -4,30 +4,7 @@ import { LucidePanelsLeftBottom, Linkedin, LucideAngularModule, Youtube, MailIco
 @Component({
   imports: [LucideAngularModule],
   templateUrl: './contact-page.component.html',
-  styles: `
-  .contact-card::before {
-            content: '';
-            position: absolute;
-            inset: 0;
-            border-radius: 1.5rem;
-            padding: 2px;
-            background: linear-gradient(45deg, transparent, #5025d1, #cad5e2, transparent);
-            -webkit-mask: linear-gradient(#fff 0 0) content-box, linear-gradient(#fff 0 0);
-            mask: linear-gradient(#fff 0 0) content-box, linear-gradient(#fff 0 0);
-            -webkit-mask-composite: xor;
-            mask-composite: exclude;
-            opacity: 0;
-            transition: opacity 0.3s ease;
-        }
-
-        .contact-card:hover::before {
-            opacity: 1;
-        }
-
-        .contact-card:hover .icon-wrapper {
-            transform: rotate(-10deg) scale(1.1);
-        }
-  `
+  styleUrl: './contact-page.component.css'
 })
 export default class ContactPageComponent {
   linkedin = computed(() => Linkedin);
