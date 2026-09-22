a035e0fb43c677856706d4fe9ef077fdd4050fb1
euclidesseg <euclides2696@gmail.com>
2026-09-21 22:39:30 -0500

    update contacts page

--- src/app/Euclides/pages/contact-page/contact-page.component.ts
+++ src/app/Euclides/pages/contact-page/contact-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { LucidePanelsLeftBottom, Linkedin, LucideAngularModule, Youtube, MailIcon, GithubIcon } from 'lucide-angular';
 
 @Component({
@@ -7,10 +7,21 @@ import { LucidePanelsLeftBottom, Linkedin, LucideAngularModule, Youtube, MailIco
   styleUrl: './contact-page.component.css'
 })
 export default class ContactPageComponent {
+
   linkedin = computed(() => Linkedin);
   youtube = computed(() => Youtube);
   email = computed(() => MailIcon);
   github = computed(() => GithubIcon);
 
+  copied = signal(false);
 
+  async copyEmail(): Promise<void> {
+    await navigator.clipboard.writeText('euclides2696@gmail.com');
+
+    this.copied.set(true);
+
+    setTimeout(() => {
+      this.copied.set(false);
+    }, 2000);
+  }
 }
