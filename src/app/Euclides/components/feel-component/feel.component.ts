c1619376c7800c1d6c1540d53c9d3e9caf31081c
euclidesseg <euclides2696@gmail.com>
2026-09-23 23:58:11 -0500

    add feel skill

---
+++ src/app/Euclides/components/feel-component/feel.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-feel-component',
+  imports: [],
+  templateUrl: './feel.component.html',
+  styleUrl: './feel.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class FeelComponent {}
--- src/app/Euclides/pages/about-page/about-page.component.ts
+++ src/app/Euclides/pages/about-page/about-page.component.ts
@@ -3,9 +3,10 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { FocusAreasComponent } from "../../components/focus-areas/focus-areas.component";
 import { StatisticsComponent } from '../../components/statistics-component/statistics.component';
 import { RouterLink } from '@angular/router';
+import { FeelComponent } from '../../components/feel-component/feel.component';
 
 @Component({
-  imports: [UpperCasePipe, FocusAreasComponent, StatisticsComponent, RouterLink],
+  imports: [UpperCasePipe, FocusAreasComponent, StatisticsComponent, RouterLink, FeelComponent],
   templateUrl: './about-page.component.html',
   styleUrl:'./about-page.component.css'
 })
